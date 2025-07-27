681c662b8a716a3de6adf7c27c8a4db229842bb6
Sendy2626 <sendy.hartono@gmail.com>
2025-07-27 22:41:36 +0700

    Enter table2

---
+++ Tables/dbo.table2.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[table2] (
+  [id] [int] IDENTITY,
+  [name] [varchar](50) NULL,
+  [phone] [varchar](50) NULL,
+  [email] [varchar](50) NULL,
+  CONSTRAINT [PK_table2_id] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
